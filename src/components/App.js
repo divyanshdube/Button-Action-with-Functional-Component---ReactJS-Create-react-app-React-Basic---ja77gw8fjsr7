9f0a0172c9e266d181d4f70ca72301f53ad24ed1
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-09-24 14:51:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,25 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  
+  const [setShown, isSetShown] = useState(false);
+
+  function onClickHandler() {
+    isSetShown(true);
+  }
+  
   return (
     <div id="main">
       // Do not alter the main div
+   <button id="click" onClick={onClickHandler}>
+        Click Me!
+      </button>
+      {setShown && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
